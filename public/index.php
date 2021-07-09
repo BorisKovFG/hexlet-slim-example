e0d122d7e5a728a45acdcdec05e3175b901efb86
BorisKovFG <multiallin@gmail.com>
2021-07-09 13:11:54 +0400

    add generator for task and page companies with pager

--- public/index.php
+++ public/index.php
@@ -4,12 +4,12 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
+$companies = \App\Generator::generate(100);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Hello, world!');
-    //$response2 = $response->withStatus(302);
     return $response;
 });
 
@@ -27,5 +27,11 @@ $app->post('/users', function ($request, $response) {
 $app->post("/about", function ($request, $response) {
     return $response->write("POST /about");
 });
-
+$app->get("/companies", function ($request, $response) use ($companies) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 5);
+    $interval = ($page - 1) * $per;
+    $response->getBody()->write(json_encode(array_slice($companies, $interval, $per)));
+    return $response;
+});
 $app->run();
