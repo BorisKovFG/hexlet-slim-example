be77f86880ca91763ba53dcb49e71c2bc4380450
BorisKovFG <multiallin@gmail.com>
2021-07-09 12:30:46 +0400

    add Makefile and experemnted with routing

--- public/index.php
+++ public/index.php
@@ -8,7 +8,24 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
+    $response->getBody()->write('Hello, world!');
+    //$response2 = $response->withStatus(302);
     return $response;
 });
+
+$app->get("/about", function ($request, $response) {
+    return $response->write("GET /about");
+});
+$app->get('/users', function ($request, $response) {
+    $page = $request->getQueryParam('page', 1);
+    $per = $request->getQueryParam('per', 10);
+    return $response;
+});
+$app->post('/users', function ($request, $response) {
+    return $response->withStatus(302);
+});
+$app->post("/about", function ($request, $response) {
+    return $response->write("POST /about");
+});
+
 $app->run();
